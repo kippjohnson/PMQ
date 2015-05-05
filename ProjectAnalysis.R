93fccda4c1676ec46367180f2dbb54606e736b64
Kipp Johnson <kipp.william.johnson@gmail.com>
2015-05-05 14:19:33 -0400

    Added ANOVA and collapsed T-test analysis for questions 13-25.

--- ProjectAnalysis.R
+++ ProjectAnalysis.R
@@ -36,7 +36,7 @@ table(infile$Q29.1)
 # Q30.1 = research interest (1=yes)
 table(infile$Q30.1)
 
-### ACtually do demographics
+### Actually do demographics
 nMales = table(infile$Q27)[[1]]
 nFemales = table(infile$Q27)[[2]]
 nNoSex = table(infile$Q27,exclude=NULL)[[3]]
@@ -58,20 +58,19 @@ demog <- mutate(demog, PercentTotal = demog[,1] / 212)
 
 kable(demog, digits=2)
 
-
-
 ### Space to print a demographics table
 
 #Because questions 2,4,5,6 and reversed on our likert scale, we need to convert them back to have
 # the same meaning (i.e., change 5-->1 , 3-->3, and 1-->5)
-divergence_corrected <- data.frame(c( abs( infile[,c(3,5,6,9)])))
-infile1 <- data.frame(c(infile[,c(2,4,7,8,10,11,12,13)] ,divergence_corrected))
+  #divergence_corrected <- data.frame(c( abs( infile[,c(3,5,6,9)])))
+  #infile1 <- data.frame(c(infile[,c(2,4,7,8,10,11,12,13)] ,divergence_corrected))
+infile1 <- infile
 
 ### Compute EBPAS Scores
 # First, add a column with the total EBPAS Score
 # This is calculated by summing the first 12 questions
 infile2 <- mutate(infile1, EBPAS = Q1+Q2+Q3+Q4+Q5+Q6+Q7+Q8+Q9+Q10+Q11+Q12)
-length(which(is.na(infile2$EBPAS))) # There are 18 people who did not completely fill out the survey
+length(which(is.na(infile2$EBPAS))) # There are 17 people who did not completely fill out the survey
 
 #Generate a new file (infile3), completely dropping those observations who did not fill out the entirety of Questions 1-12
 infile3 <- infile2[-which(is.na(infile2$EBPAS)),]
@@ -123,14 +122,229 @@ table(infile$Q24) # Ability to explain genomic test results to patients
 table(infile$Q25) # Ability to make treatment recommendations based upon genomic test results
 
 
-### T Tests
+### T Tests / ANOVAs
 
+### Q13
+#Q13 T-test
 t.test(infile3$EBPAS ~ infile3$Q13) #NS
 
-summary(aov(infile3$EBPAS ~ (infile3$Q14))) #NS
+### Q14
+#Q14 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q14))) #NS
+  TukeyHSD(aov(infile3$EBPAS~ as.factor(infile3$Q14)))
   boxplot(EBPAS ~ Q14, data=infile3)
 
-summary(aov(infile3$EBPAS ~ (infile3$Q15))) #significant
+#Q14 Collapsed t-test
+infile3$Q14c <- infile3$Q14
+infile3$Q14c[infile3$Q14c==3] <- 2
+t.test(infile3$EBPAS ~ infile3$Q14c) # Significant
+CT14 <- t.test(infile3$EBPAS ~ infile3$Q14c) # Significant)
+
+
+### Q15
+# Q15 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q15))) #significant
+  TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q15)))
   boxplot(EBPAS ~ Q15, data=infile3)
 
+#Q15 Collapsed t-test
+infile3$Q15c <- infile3$Q15
+infile3$Q15c[infile3$Q15c==2] <- 1
+infile3$Q15c[infile3$Q15c==3] <- 1
+#infile3$Q15c[infile3$Q15c==3] <- 2
+infile3$Q15c[infile3$Q15c==4] <- 2
+infile3$Q15c[infile3$Q15c==5] <- 2
+
+table(infile3$Q15)
+table(infile3$Q15c)
+t.test(infile3$EBPAS ~ infile3$Q15c) #Significant Note: depends upon assignment of category 3
+CT15 <- t.test(infile3$EBPAS ~ infile3$Q15c) # Significant)
+
+### Q16
+#Q16 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q16))) #significant
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q16)))
+boxplot(EBPAS ~ Q16, data=infile3)
+
+#Q16 Collapsed t-test
+infile3$Q16c <- infile3$Q16
+infile3$Q16c[infile3$Q16c==2] <- 1
+infile3$Q16c[infile3$Q16c==3] <- 1
+infile3$Q16c[infile3$Q16c==4] <- 2
+infile3$Q16c[infile3$Q16c==5] <- 2
+
+table(infile3$Q16)
+table(infile3$Q16c)
+t.test(infile3$EBPAS ~ infile3$Q16c) # NS
+CT16 <- t.test(infile3$EBPAS ~ infile3$Q16c) # Significant)
+
+### Q17
+#Q17 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q17)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q17)))
+boxplot(EBPAS ~ Q17, data=infile3)
+
+#Q17 Collapsed t-test
+infile3$Q17c <- infile3$Q17
+infile3$Q17c[infile3$Q17c==2] <- 1
+infile3$Q17c[infile3$Q17c==3] <- 1
+infile3$Q17c[infile3$Q17c==4] <- 2
+infile3$Q17c[infile3$Q17c==5] <- 2
+
+table(infile3$Q17)
+table(infile3$Q17c)
+t.test(infile3$EBPAS ~ infile3$Q17c) # NS
+CT17 <- t.test(infile3$EBPAS ~ infile3$Q17c) # Significant)
+
+### Q18
+#Q18 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q18)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q18)))
+boxplot(EBPAS ~ Q18, data=infile3)
+
+#Q18 Collapsed t-test
+infile3$Q18c <- infile3$Q18
+table(infile3$Q18)
+
+infile3$Q18c[infile3$Q18c==2] <- 1
+infile3$Q18c[infile3$Q18c==3] <- 1
+infile3$Q18c[infile3$Q18c==4] <- 2
+infile3$Q18c[infile3$Q18c==5] <- 2
+
+table(infile3$Q18c)
+t.test(infile3$EBPAS ~ infile3$Q18c) # Significant
+CT18 <- t.test(infile3$EBPAS ~ infile3$Q18c) # Significant)
+
+### Q19
+#Q19 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q19)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q19)))
+boxplot(EBPAS ~ Q19, data=infile3)
+
+#Q19 Collapsed t-test
+infile3$Q19c <- infile3$Q19
+table(infile3$Q19)
+
+infile3$Q19c[infile3$Q19c==2] <- 1
+infile3$Q19c[infile3$Q19c==3] <- 1
+infile3$Q19c[infile3$Q19c==4] <- 2
+infile3$Q19c[infile3$Q19c==5] <- 2
+
+table(infile3$Q19c)
+t.test(infile3$EBPAS ~ infile3$Q19c) # Significant
+CT19 <- t.test(infile3$EBPAS ~ infile3$Q19c) # Significant)
+
+### Q20
+#Q20 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q20)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q20)))
+boxplot(EBPAS ~ Q20, data=infile3)
+
+#Q20 Collapsed t-test
+infile3$Q20c <- infile3$Q20
+table(infile3$Q20)
+
+infile3$Q20c[infile3$Q20c==2] <- 1
+infile3$Q20c[infile3$Q20c==3] <- 1
+infile3$Q20c[infile3$Q20c==4] <- 2
+infile3$Q20c[infile3$Q20c==5] <- 2
+
+table(infile3$Q20c)
+t.test(infile3$EBPAS ~ infile3$Q20c) # Significant
+CT20 <- t.test(infile3$EBPAS ~ infile3$Q20c) # Significant)
+
+### Q21
+#Q21 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q21)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q21)))
+boxplot(EBPAS ~ Q21, data=infile3)
+
+#Q21 Collapsed t-test
+infile3$Q21c <- infile3$Q21
+table(infile3$Q21)
+
+infile3$Q21c[infile3$Q21c==2] <- 1
+infile3$Q21c[infile3$Q21c==3] <- 1
+infile3$Q21c[infile3$Q21c==4] <- 2
+infile3$Q21c[infile3$Q21c==5] <- 2
+
+table(infile3$Q21c)
+t.test(infile3$EBPAS ~ infile3$Q21c) # Significant)
+CT21 <- t.test(infile3$EBPAS ~ infile3$Q21c) # Significant)
+
+### Q22
+#Q22 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q22)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q22)))
+boxplot(EBPAS ~ Q22, data=infile3)
+
+#Q22 Collapsed t-test
+infile3$Q22c <- infile3$Q22
+table(infile3$Q22)
+
+infile3$Q22c[infile3$Q22c==2] <- 1
+infile3$Q22c[infile3$Q22c==3] <- 1
+infile3$Q22c[infile3$Q22c==4] <- 2
+infile3$Q22c[infile3$Q22c==5] <- 2
+
+table(infile3$Q22c)
+t.test(infile3$EBPAS ~ infile3$Q22c) # Significant)
+CT22 <- t.test(infile3$EBPAS ~ infile3$Q22c) # Significant)
+
+### Q23
+#Q23 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q23)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q23)))
+boxplot(EBPAS ~ Q23, data=infile3)
+
+#Q23 Collapsed t-test
+infile3$Q23c <- infile3$Q23
+table(infile3$Q23)
+
+infile3$Q23c[infile3$Q23c==2] <- 1
+infile3$Q23c[infile3$Q23c==3] <- 1
+infile3$Q23c[infile3$Q23c==4] <- 2
+infile3$Q23c[infile3$Q23c==5] <- 2
+
+table(infile3$Q23c)
+t.test(infile3$EBPAS ~ infile3$Q23c) # Significant)
+CT23 <- t.test(infile3$EBPAS ~ infile3$Q23c) # Significant)
+
+### Q24
+#Q24 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q24)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q24)))
+boxplot(EBPAS ~ Q24, data=infile3)
+
+#Q24 Collapsed t-test
+infile3$Q24c <- infile3$Q24
+table(infile3$Q24)
+
+infile3$Q24c[infile3$Q24c==2] <- 1
+infile3$Q24c[infile3$Q24c==3] <- 1
+infile3$Q24c[infile3$Q24c==4] <- 2
+infile3$Q24c[infile3$Q24c==5] <- 2
+
+table(infile3$Q24c)
+t.test(infile3$EBPAS ~ infile3$Q24c) # Significant)
+CT24 <- t.test(infile3$EBPAS ~ infile3$Q24c) # Significant)
+
+### Q25
+#Q25 ANOVA
+summary(aov(infile3$EBPAS ~ as.factor(infile3$Q25)))
+TukeyHSD(aov(infile3$EBPAS ~ as.factor(infile3$Q25)))
+boxplot(EBPAS ~ Q25, data=infile3)
+
+#Q25 Collapsed t-test
+infile3$Q54c <- infile3$Q25
+table(infile3$Q25)
+
+infile3$Q25c[infile3$Q25c==2] <- 1
+infile3$Q25c[infile3$Q25c==3] <- 1
+infile3$Q25c[infile3$Q25c==4] <- 2
+infile3$Q25c[infile3$Q25c==5] <- 2
+
+table(infile3$Q25c)
+t.test(infile3$EBPAS ~ infile3$Q25c) # Significant)
+CT25 <- t.test(infile3$EBPAS ~ infile3$Q25c) # Significant)
 
